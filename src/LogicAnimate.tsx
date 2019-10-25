9508eea3ca577d6971cb4e0e0e8a187e5b437437
EmilsWebbod <emil@webbod.no>
2019-10-25 10:50:47 +0200

    Make animateProps not necessary

--- src/LogicAnimate.tsx
+++ src/LogicAnimate.tsx
@@ -2,16 +2,25 @@ import * as React from 'react';
 import { useRef } from 'react';
 import { useLogicAnimate, UseLogicAnimateProps } from './useLogicAnimate';
 
-interface Props extends  React.HTMLAttributes<HTMLDivElement> {
-  animateProps: UseLogicAnimateProps;
+interface Props extends React.HTMLAttributes<HTMLDivElement> {
+  animateProps?: UseLogicAnimateProps;
 }
 
 export type LogicAnimateProps = UseLogicAnimateProps &
-  JSX.ElementChildrenAttribute & Props;
+  JSX.ElementChildrenAttribute &
+  Props;
 
-export function LogicAnimate({ children, animateProps, ...props }: LogicAnimateProps) {
+export function LogicAnimate({
+  children,
+  animateProps,
+  ...props
+}: LogicAnimateProps) {
   const ref = useRef<HTMLDivElement>(null);
   useLogicAnimate(ref.current, { ...animateProps });
 
-  return <div ref={ref} {...props}>{children}</div>;
+  return (
+    <div ref={ref} {...props}>
+      {children}
+    </div>
+  );
 }
