7a687b21476cfa35a116af99c40de322c00e124e
EmilsWebbod <emil@webbod.no>
2019-10-25 10:46:16 +0200

    v2.0.0

--- src/LogicAnimate.tsx
+++ src/LogicAnimate.tsx
@@ -2,12 +2,16 @@ import * as React from 'react';
 import { useRef } from 'react';
 import { useLogicAnimate, UseLogicAnimateProps } from './useLogicAnimate';
 
+interface Props extends  React.HTMLAttributes<HTMLDivElement> {
+  animateProps: UseLogicAnimateProps;
+}
+
 export type LogicAnimateProps = UseLogicAnimateProps &
-  JSX.ElementChildrenAttribute;
+  JSX.ElementChildrenAttribute & Props;
 
-export function LogicAnimate({ children, ...props }: LogicAnimateProps) {
+export function LogicAnimate({ children, animateProps, ...props }: LogicAnimateProps) {
   const ref = useRef<HTMLDivElement>(null);
-  useLogicAnimate(ref.current, { ...props });
+  useLogicAnimate(ref.current, { ...animateProps });
 
-  return <div ref={ref}>{children}</div>;
+  return <div ref={ref} {...props}>{children}</div>;
 }
